094d8cc27c2cce6ca969faa0cd1fd1e1b3e83ca5
Nate Nystrom <nate.nystrom@gmail.com>
2019-04-09 20:48:14 +0200

    removed dependency on Prelude in TIMain. removed TypingHaskellInHaskell.

--- src/TIMain.hs
+++ src/TIMain.hs
@@ -25,7 +25,6 @@ import TIMonad
 import Infer
 import Lit
 import Pat
-import StaticPrelude
 
 -----------------------------------------------------------------------------
 
@@ -36,55 +35,7 @@ data Expr = Var   Id
           | Let   BindGroup Expr
 
           | Lam   Alt
-          | If    Expr Expr Expr
           | Case  Expr [(Pat,Expr)]
-
------------------------------------------------------------------------------
--- The following helper functions are used to construct sample programs; if we
--- change the representation of Expr above, then we need only change the
--- definitions of the following combinators to match, and do not need to
--- rewrite all the test code.
-
-ap              = foldl1 Ap
-evar v          = (Var v)
-elit l          = (Lit l)
-econst c        = (Const c)
-elet e f        = foldr Let f (map toBg e)
-
-toBg           :: [(Id, Maybe Scheme, [Alt])] -> BindGroup
-toBg g          = ([(v, t, alts) | (v, Just t, alts) <- g ],
-                   filter (not . null) [[(v,alts) | (v,Nothing,alts) <- g]])
-
-pNil            = PCon nilCfun []
-pCons x y       = PCon consCfun [x,y]
-
-eNil            = econst nilCfun
-eCons x y       = ap [ econst consCfun, x, y ]
-
-{-
-ecase           = Case
-elambda         = Lam
-eif             = If
--}
-
-ecase d as      = elet [[ ("_case",
-                           Nothing,
-                           [([p],e) | (p,e) <- as]) ]]
-                       (ap [evar "_case", d])
-eif c t f       = ecase c [(PCon trueCfun [], t),(PCon falseCfun [], f)]
-elambda alt     = elet [[ ("_lambda",
-                           Nothing,
-                           [alt]) ]]
-                             (evar "_lambda")
-eguarded :: [(Expr, Expr)] -> Expr
-eguarded        = foldr (\(c,t) e -> eif c t e) efail
-efail           = Const ("FAIL" :>: Forall [Star] ([] :=> TGen 0))
-esign e t       = elet [[ ("_val", Just t, [([],e)]) ]] (evar "_val")
-
-eCompFrom p e c = ap [ econst mbindMfun, e, elambda ([p],c) ]
-eCompGuard e c  = eif e c eNil
-eCompLet bgs c  = elet bgs c
-eListRet e      = eCons e eNil
 
 -----------------------------------------------------------------------------
 
@@ -107,13 +58,6 @@ tiExpr ce as (Let bg e)       = do (ps, as') <- tiBindGroup ce as bg
 
 tiExpr ce as (Lam alt)
   = tiAlt ce as alt
-tiExpr ce as (If e e1 e2)
-  = do (ps,t)   <- tiExpr ce as e
-       unify t tBool
-       (ps1,t1) <- tiExpr ce as e1
-       (ps2,t2) <- tiExpr ce as e2
-       unify t1 t2
-       return (ps++ps1++ps2, t1)
 tiExpr ce as (Case e branches)
   = do (ps, t) <- tiExpr ce as e
        v       <- newTVar Star
